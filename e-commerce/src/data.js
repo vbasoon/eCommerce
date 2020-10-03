b6ea670b2ced943d06f69e5ce5f7832720d2ccf8
Басун Володимир <vbasoon@gmail.com>
2020-10-03 12:35:54 +0300

    array data

--- e-commerce/src/data.js
+++ e-commerce/src/data.js
@@ -1,5 +1,58 @@
-export default const data = {
-   products: [{
-      
-   }]
-}
+export default data = {
+  products: [
+    {
+      name: "Slim Shirt",
+      category: "Shirts",
+      image: "./images/1.jpg",
+      price: 60,
+      brand: "Levis",
+      rating: 4.5,
+      reviews: 10,
+    },
+    {
+      name: "Fit Shirt",
+      category: "Shirts",
+      image: "./images/1.jpg",
+      price: 50,
+      brand: "Levis",
+      rating: 4.2,
+      reviews: 5,
+    },
+    {
+      name: "Blouse",
+      category: "Blouse"
+      image: "./images/1.jpg",
+      price: 60,
+      brand: "Levis",
+      rating: 4.5,
+      reviews: 10,
+    },
+    {
+      name: "Best Pants",
+      category: "Pants",
+      image: "./images/1.jpg",
+      price: 70,
+      brand: "Levis",
+      rating: 4.5,
+      reviews: 10,
+    },
+    {
+      name: "Light Short",
+      category: "Sorts"
+      image: "./images/1.jpg",
+      price: 60,
+      brand: "Levis",
+      rating: 4.5,
+      reviews: 10,
+    },
+    {
+      name: "Rock Jeans",
+      category: "Jeans",
+      image: "./images/1.jpg",
+      price: 60,
+      brand: "Levis",
+      rating: 4.5,
+      reviews: 10,
+    },
+  ],
+};
