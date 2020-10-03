820b0f4780e05779a1ef1d521f046f272596e7db
Басун Володимир <vbasoon@gmail.com>
2020-10-03 10:32:47 +0300

    close button

---
+++ template/main.js
@@ -0,0 +1,8 @@
+const openSidebar = () => {
+  document.querySelector(".sidebar").classList.add("open");
+  console.log("work");
+};
+
+const closeSidebar = () => {
+  document.querySelector(".sidebar").classList.remove("open");
+};
