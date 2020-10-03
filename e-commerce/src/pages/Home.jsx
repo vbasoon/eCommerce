917003ba85cccfc6198290dc027100872946d9b5
Басун Володимир <vbasoon@gmail.com>
2020-10-03 14:14:57 +0300

    move ul to home

--- e-commerce/src/pages/Home.jsx
+++ e-commerce/src/pages/Home.jsx
@@ -1,9 +1,30 @@
 import React from "react";
+import data from "../data";
 
 const Home = (props) => {
   return (
     <div>
-      <h1>Home page</h1>
+      <ul className="products-list">
+        {data.products.map((product) => (
+          <li key={product.id}>
+            <div className="product-card">
+              <img
+                className="product-card__image"
+                src={product.image}
+                alt={product.name}
+              />
+              <div className="product-card__name">
+                <a href="product.html">{product.name}</a>
+              </div>
+              <div className="product-card__brand">{product.brand}</div>
+              <div className="product-card__price">{product.price}</div>
+              <div className="product-card__rating">
+                {product.rating} Stars ({product.reviews} Reviews)
+              </div>
+            </div>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 };
