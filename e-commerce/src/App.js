bc48408d5d7ef9e902a76348f489a38fa5cd458c
Басун Володимир <vbasoon@gmail.com>
2020-10-03 14:21:44 +0300

    layout product page

--- e-commerce/src/App.js
+++ e-commerce/src/App.js
@@ -20,7 +20,7 @@ const App = () => {
         <header className="header">
           <div className="logo">
             <button onClick={openSidebar}>&#9776;</button>
-            <a href="index.html">Logo</a>
+            <a href="/">Logo</a>
           </div>
           <nav></nav>
           <div className="header-links">
@@ -35,10 +35,10 @@ const App = () => {
           </button>
           <ul>
             <li>
-              <a href="index.html">Pants</a>
+              <a href="/">Pants</a>
             </li>
             <li>
-              <a href="index.html">Shirts</a>
+              <a href="/">Shirts</a>
             </li>
           </ul>
         </aside>
@@ -46,27 +46,6 @@ const App = () => {
           <div className="content">
             <Route path="/" exact={true} component={Home} />
             <Route path="/products/:id" exact component={Product} />
-            <ul className="products-list">
-              {data.products.map((product) => (
-                <li key={product.id}>
-                  <div className="product-card">
-                    <img
-                      className="product-card__image"
-                      src={product.image}
-                      alt={product.name}
-                    />
-                    <div className="product-card__name">
-                      <a href="product.html">{product.name}</a>
-                    </div>
-                    <div className="product-card__brand">{product.brand}</div>
-                    <div className="product-card__price">{product.price}</div>
-                    <div className="product-card__rating">
-                      {product.rating} Stars ({product.reviews} Reviews)
-                    </div>
-                  </div>
-                </li>
-              ))}
-            </ul>
           </div>
         </main>
         <footer className="footer">
